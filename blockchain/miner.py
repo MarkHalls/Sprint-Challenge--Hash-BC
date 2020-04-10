9411eb99a897b01419b80b6483a8507423df69e2
Mark Halls <mark.halls@gmail.com>
2020-04-10 09:02:12 -0700

    Add proof of work using multi-ouroboros

--- blockchain/miner.py
+++ blockchain/miner.py
@@ -24,7 +24,8 @@ def proof_of_work(last_proof):
 
     print("Searching for next proof")
     proof = 0
-    #  TODO: Your code here
+    while not valid_proof(last_proof, proof):
+        proof = random.randint(0, 2147483647)
 
     print("Proof found: " + str(proof) + " in " + str(timer() - start))
     return proof
@@ -38,12 +39,12 @@ def valid_proof(last_hash, proof):
 
     IE:  last_hash: ...AE912345, new hash 12345E88...
     """
+    hash_last = hashlib.sha256(f"{last_hash}".encode()).hexdigest()
+    my_proof = hashlib.sha256(f"{proof}".encode()).hexdigest()
+    return hash_last[-5:] == my_proof[:5]
 
-    # TODO: Your code here!
-    pass
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     # What node are we interacting with?
     if len(sys.argv) > 1:
         node = sys.argv[1]
@@ -58,7 +59,7 @@ if __name__ == '__main__':
     print("ID is", id)
     f.close()
 
-    if id == 'NONAME\n':
+    if id == "NONAME\n":
         print("ERROR: You must change your name in `my_id.txt`!")
         exit()
     # Run forever until interrupted
@@ -66,15 +67,14 @@ if __name__ == '__main__':
         # Get the last proof from the server
         r = requests.get(url=node + "/last_proof")
         data = r.json()
-        new_proof = proof_of_work(data.get('proof'))
+        new_proof = proof_of_work(data.get("proof"))
 
-        post_data = {"proof": new_proof,
-                     "id": id}
+        post_data = {"proof": new_proof, "id": id}
 
         r = requests.post(url=node + "/mine", json=post_data)
         data = r.json()
-        if data.get('message') == 'New Block Forged':
+        if data.get("message") == "New Block Forged":
             coins_mined += 1
             print("Total coins mined: " + str(coins_mined))
         else:
-            print(data.get('message'))
+            print(data.get("message"))
